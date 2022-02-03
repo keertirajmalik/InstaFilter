111f48d13ab77a1f556a1e668448dfde6f3744a5
keertirajmalik <keertirajmalik@gmail.com>
2022-02-03 20:38:27 +0530

    added alert to display when user save without importing image

--- Project 13/ViewController.swift
+++ Project 13/ViewController.swift
@@ -78,7 +78,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     }
     
     @IBAction func save(_ sender: Any) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let ac = UIAlertController(title: "No Image Imported", message: "Import image and Apply filters before saving.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            return
+        }
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
     
