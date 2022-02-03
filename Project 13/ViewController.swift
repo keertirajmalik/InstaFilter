577f0ef81dcf40d178adff35db78ac7e55f7bd69
keertirajmalik <keertirajmalik@gmail.com>
2022-02-03 20:34:49 +0530

    Saving the updated image

--- Project 13/ViewController.swift
+++ Project 13/ViewController.swift
@@ -78,7 +78,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     }
     
     @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
+    
     @IBAction func intensityChanged(_ sender: Any) {
         applyProcessing()
     }
@@ -107,6 +110,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
             let processedImage = UIImage(cgImage: cgImage)
             imageView.image = processedImage
+        }
+    }
+    
+    @objc
+    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+        } else  {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
         }
     }
 }
